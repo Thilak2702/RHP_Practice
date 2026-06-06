1ec7ab8ba70626d35848c721891434e8772eb3a3
Thilak C <thilak.chandramohan27@gmail.com>
2026-06-06 15:06:21 +0530

    Add function to check string combinations

---
+++ To_check_string_contains0-9.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+void solve(string s){
+
+   int l=s.size();
+
+   for(int c=1;c<=(1<<l)-1;c++){
+    string st="";
+
+    for(int i=0;i<l;i++){
+        if(c&(1<<i)){
+            st+=s[i];
+        }
+    }
+    cout << st << endl;
+   }
+}
+
+int main(){
+    string s;
+
+    cout << "Enter the string: ";
+    cin >> s;
+
+    solve(s);
+    return 0;
+
+}
